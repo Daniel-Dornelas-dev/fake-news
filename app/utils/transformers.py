2c643d2ef6656a5cfeb88dbf6879f03420c8b3e8
Daniel-Dornelas-dev <daniel.santos212@fatec.sp.gov.br>
2025-07-03 22:14:33 -0300

    ajustando erros

--- app/utils/transformers.py
+++
@@ -1,7 +0,0 @@
-# app/utils/transformers.py
-
-def select_text_column(dataframe):
-    return dataframe["text"]
-
-def select_sentiment_column(dataframe):
-    return dataframe["sentimento"].values.reshape(-1, 1)
